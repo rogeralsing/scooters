ee82dc8e21efec5953a4579d63499ac50f13a543
rogeralsing <rogeralsing@gmail.com>
2018-04-17 22:23:45 +0200

    .

--- src/main/kotlin/backend/ScooterController.kt
+++ src/main/kotlin/backend/ScooterController.kt
@@ -4,7 +4,7 @@ import domain.runSimulation
 import domain.scootersOf
 import org.springframework.web.bind.annotation.*
 
-data class Request(val C:Int,val P:Int, val scooters: IntArray)
+data class Request(val C:Int,val P:Int, val scooters: List<Int>)
 data class Response(val fleet_engineers: Int)
 
 @RestController
@@ -12,7 +12,7 @@ class ScooterController {
     //@GetMapping("/greeting")
     @PostMapping("/scooters")
     fun greeting(@RequestBody request: Request) : Response {
-        val scooters = scootersOf(*request.scooters)
+        val scooters = scootersOf(*request.scooters.toIntArray())
         val res = runSimulation(request.C, request.P, scooters)
         return Response(res)
     }
--- src/main/kotlin/backend/main.kt
+++ src/main/kotlin/backend/main.kt
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 
+//configure object mapper so that SpringBoot understands Kotlin types
 @Configuration
 open class ObjectMapperConfiguration {
     @Bean
