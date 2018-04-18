1ca95e29940c0c32b10dbb0b2c56a766f7d946b0
rogeralsing <rogeralsing@gmail.com>
2018-04-18 18:20:09 +0200

    replaced Pair with Domain class

--- src/main/kotlin/domain/domain.kt
+++ src/main/kotlin/domain/domain.kt
@@ -1,5 +1,6 @@
 package domain
 
+data class District(val requiredEngineers:Int,val requiredEngineersWithFM:Int)
 // Domain model
 //
 // The task contains no requirements on performance nor memory footprint, the requirements also states low numbers for size and ranges.
@@ -16,18 +17,22 @@ fun runSimulation(fleetManagerCapacity: Int, fleetEngineerCapacity: Int, scooter
         else -> {
 
             //helper function to calculate the required engineers in a district
-            val requiredFleetEngineers = { scootersInDistrict: Int -> Math.ceil(scootersInDistrict.toDouble() / fleetEngineerCapacity).toInt() }
+            val requiredEngineers = { scootersInDistrict: Int -> Math.ceil(scootersInDistrict.toDouble() / fleetEngineerCapacity).toInt() }
 
             // step 1, first scan calculates the needed FEs per district 1. if the FM is not there, 2. if the FM is there.
-            // so we have a sequence of pairs with the two values per district
-            val precalculated = scooters.map { Pair(requiredFleetEngineers(it), requiredFleetEngineers(Math.max(0, it - fleetManagerCapacity))) }
+            // so we have a sequence of districts with the two values per district
+            // Math.max prevents negative values in case the district has fewer scooters than the FMs capacity
+            val districts = scooters.map { District(requiredEngineers(it), requiredEngineers(Math.max(0, it - fleetManagerCapacity))) }
 
-            // step 2, caclulates a speculative total. this is the sum of the required engineers without taking the FM into account
-            val speculativeResult = precalculated.sumBy { it.first }
+            // step 2, calculates a speculative total. this is the sum of the required engineers without taking the FM into account
+            val speculativeResult = districts.sumBy { it.requiredEngineers }
 
-            // the final step 3, then folds over the precalculated sequence and evaluates the required engineers if the manager is in each district.
+            // the final step 3, then folds over the requiredEngineersPerDistrict sequence and evaluates the required engineers if the manager is in each district.
             // reduces by returning the smallest number for each fold
-            return precalculated.fold(Int.MAX_VALUE, { bestResult, pair -> Math.min(bestResult, speculativeResult - (pair.first - pair.second)) })
+            // this is a single iteration over the sequence, we get the delta between first and second of the pair
+            // this delta is subtracted from the speculative result, this give us the result when the FM is in this district
+            // the resulting value is then compared to the bestResult
+            return districts.fold(Int.MAX_VALUE, { bestResult, district -> Math.min(bestResult, speculativeResult - (district.requiredEngineers - district.requiredEngineersWithFM)) })
         }
     }
 }
