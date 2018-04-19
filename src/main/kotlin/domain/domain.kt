88cb1266b76688ed40df5389d65464f7d8c00d9c
rogeralsing <rogeralsing@gmail.com>
2018-04-20 01:21:55 +0200

    use require instead of pattern matching

--- src/main/kotlin/domain/domain.kt
+++ src/main/kotlin/domain/domain.kt
@@ -10,45 +10,39 @@ data class District(val requiredEngineers: Int, val requiredEngineersWithFM: Int
 // This solution runs in O(n) time
 // (As opposed to the naive approach to brute force using O(n²) time)
 fun runSimulation(fleetManagerCapacity: Int, fleetEngineerCapacity: Int, scooters: IntArray): Int {
-    when {
-        fleetManagerCapacity !in 1..999 -> throw IllegalArgumentException("fleetManagerCapacity must be between 1 and 999")
-        fleetEngineerCapacity !in 1..1000 -> throw IllegalArgumentException("fleetEngineerCapacity must be between 1 and 1000")
-        scooters.count() !in 1..100 -> throw IllegalArgumentException("scooters count must be between 1 and 100")
-        scooters.any { it !in 0..1000 } -> throw IllegalArgumentException("scooters elements must be between 0 and 1000")
-        else -> {
+    require(fleetManagerCapacity in 1..999) { "|fleetManagerCapacity| must be between 1 and 999" }
+    require(fleetEngineerCapacity in 1..1000) { "|fleetEngineerCapacity| must be between 1 and 1000" }
+    require(scooters.count() in 1..100) { "|scooters| count must be between 1 and 100" }
+    require(scooters.all { it in 0..1000 }) { "|scooters| elements must be between 0 and 1000" }
 
-            //helper function to calculate the required engineers in a district
-            val requiredEngineers = { scootersInDistrict: Int ->
-                Math.ceil(scootersInDistrict.toDouble() / fleetEngineerCapacity).toInt()
-            }
-
-            // step 1, first scan calculates the needed FEs per district
-            // 1. if the FM is not there, 2. if the FM is there.
-            // so we have a sequence of districts with the two values per district
-            // Math.max prevents negative values in case the district has fewer
-            // scooters than the FMs capacity
-            val districts = scooters.map {
-                District(requiredEngineers(it), requiredEngineers(Math.max(0, it - fleetManagerCapacity)))
-            }
-
-            // step 2, calculates a speculative total. this is the sum of
-            // the required engineers without taking the FM into account
-            val speculativeResult = districts.sumBy {
-                it.requiredEngineers
-            }
-
-            // the final step 3, then folds over the districts sequence and
-            // evaluates the required engineers if the manager is in each district.
-            // fold by returning the smallest number for each fold
-            // this is a single iteration over the sequence, we get the delta
-            // between first and second of the pair.
-            // this delta is subtracted from the speculative result, this give us
-            // the result when the FM is in this district
-            // the resulting value is then compared to the bestResult
-            return districts.fold(Int.MAX_VALUE, { bestResult, district ->
-                Math.min(bestResult,
-                        speculativeResult - (district.requiredEngineers - district.requiredEngineersWithFM))
-            })
-        }
+    //helper function to calculate the required engineers in a district
+    val requiredEngineers = { scootersInDistrict: Int ->
+        Math.ceil(scootersInDistrict.toDouble() / fleetEngineerCapacity).toInt()
     }
+
+    // step 1, first scan calculates the needed FEs per district
+    // 1. if the FM is not there, 2. if the FM is there.
+    // so we have a sequence of districts with the two values per district
+    // Math.max prevents negative values in case the district has fewer
+    // scooters than the FMs capacity
+    val districts = scooters.map {
+        District(requiredEngineers(it), requiredEngineers(Math.max(0, it - fleetManagerCapacity)))
+    }
+
+    // step 2, calculates a speculative total. this is the sum of
+    // the required engineers without taking the FM into account
+    val speculativeResult = districts.sumBy { it.requiredEngineers }
+
+    // the final step 3, then folds over the districts sequence and
+    // evaluates the required engineers if the manager is in each district.
+    // by returning the smallest number for each fold
+    // this is a single iteration over the sequence.
+    // we get the delta between requiredEngineers and requiredEngineersWithFM of the District.
+    // this delta is subtracted from the speculative result,
+    // which give us the result when the FM is in this district
+    // the resulting value is then compared and swapped with the bestResult
+    return districts.fold(Int.MAX_VALUE, { bestResult, district ->
+        Math.min(bestResult,
+                speculativeResult - (district.requiredEngineers - district.requiredEngineersWithFM))
+    })
 }
